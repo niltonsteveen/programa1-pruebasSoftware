b9039748599267fc4658181c3602626069b1abde
Maria Camila  Gomez <mariacamilagomez@MacBook-Pro-de-Maria.local>
2017-08-13 14:01:03 -0500

    Inicializando camilaTest

---
+++ Code/camilaTest.py
@@ -0,0 +1,10 @@
+import unittest
+
+from camilaCode import ReadFile
+
+class TestsRead(unittest.TestCase):
+    def setUp(self):
+        self.read = ReadFile()
+
+if __name__ == '__main__':
+    unittest.main()
