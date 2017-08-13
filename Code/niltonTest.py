e392464aae39c01ede1f840a9b842d777acb8138
nilton <nilton.velez@udea.edu.co>
2017-08-13 00:08:07 -0500

    test para que solo entren numeros y no letras u otro caracter

--- Code/niltonTest.py
+++ Code/niltonTest.py
@@ -11,6 +11,8 @@ class TestsDeviation(unittest.TestCase):
 		self.assertEqual(None,self.cd.calcularDesviacion(-1,2))
 	def testUnSoloItem(self):
 		self.assertEqual(None, self.cd.calcularDesviacion(5, 1))
+	def testCalculoDesviacionConEntradasInvalidas(self):
+		self.assertEqual(None, self.cd.calcularDesviacion('adfa',3))
 
 if __name__ == '__main__':
 	unittest.main()
