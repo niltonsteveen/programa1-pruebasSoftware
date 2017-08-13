311d618e8a9a789038f3b3af225d778e04c74fcc
Maria Camila  Gomez <mariacamilagomez@MacBook-Pro-de-Maria.local>
2017-08-13 14:34:28 -0500

    Test onlyRead

--- Code/camilaTest.py
+++ Code/camilaTest.py
@@ -5,6 +5,8 @@ from camilaCode import ReadFile
 class TestsRead(unittest.TestCase):
     def setUp(self):
         self.read = ReadFile()
+    def testLeerArchivo():
+        self.assertEqual(True,self.read.onlyRead())
 
 if __name__ == '__main__':
     unittest.main()
