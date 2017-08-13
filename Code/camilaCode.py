bc93126084ed7fab6f664bbb07c325ca784e3f54
Maria Camila  Gomez <mariacamilagomez@MacBook-Pro-de-Maria.local>
2017-08-13 16:50:20 -0500

    Code file no exist

--- Code/camilaCode.py
+++ Code/camilaCode.py
@@ -1,12 +1,16 @@
+import os
+
 class ReadFile(object):
     def __init__(self):
         self.file=None
         self.muestra=[]
     def readFile(self, nameFile):
+        if not(os.path.exists(nameFile)):
+            return False
         self.file = open(nameFile, 'r')
         for line in self.file.readlines():
             self.muestra.append(int(line))
         print self.muestra
-        return
+        return True
     def onlyRead(self):
         return True if self.file.mode == 'r' else False
