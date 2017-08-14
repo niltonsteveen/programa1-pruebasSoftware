9fff72adac137200ef99f7ba04457648efdab1d3
Maria Camila  Gomez <mariacamilagomez@MacBook-Pro-de-Maria.local>
2017-08-14 09:31:29 -0500

    Functional views and css

--- code.py
+++ code.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-	content = get_file('./Template/template.html')
-    return Response(content, mimetype="text/html")
+	return render_template('template.html')
 
 @app.route('/saludo')
 def index1():
 	return 'ejemplo'
 
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=True)
+	app.run()
