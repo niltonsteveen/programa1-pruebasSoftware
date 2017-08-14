3317843988292808dab6abe903a80b3575c01da6
nilton <nilton.velez@udea.edu.co>
2017-08-14 18:57:13 -0500

    prueba para mirar el post

--- code.py
+++ code.py
@@ -6,6 +6,9 @@ def upload():
 	if request.method == 'POST':
 		fileUpload=request.files['file']
 	return render_template('template.html')
+def index():
+	if request.method == 'GET':
+		return 'Hola mundo'
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
