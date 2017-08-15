fc95dd258d0148a5ef5b2d0470a2c556237fb3b8
nilton <nilton.velez@udea.edu.co>
2017-08-15 13:44:21 -0500

    Test division by zero

--- Code/jorgeCode.py
+++ Code/jorgeCode.py
@@ -5,15 +5,15 @@ class Mean(object):
 	def __init__(self):
 		self.media=0
 	def calculateMean(self, array):
-		print len(array)
-		for x in array:
-			numberStr=str(x)
-			if numberStr.isdigit():
-				self.media=self.media+x
-			else:
-				self.media=0
-				break
-		self.media=self.media/len(array)
+		if len(array)>0:
+			for x in array:
+				numberStr=str(x)
+				if numberStr.isdigit():
+					self.media=self.media+x
+				else:
+					self.media=0
+					break
+			self.media=self.media/len(array)
 		return self.media
 	
 
