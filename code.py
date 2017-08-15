35c803d1d64e453cc53f2e696d86b90b982ebf62
nilton <nilton.velez@udea.edu.co>
2017-08-14 20:06:35 -0500

    prueba post

--- code.py
+++ code.py
@@ -1,20 +1,26 @@
 from flask import Flask, request, render_template
 from flask import jsonify
+from werkzeug import secure_filename
 app=Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-@app.route('/' , methods=['POST', 'GET'])
+def allowed_file(filename):
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
 
+@app.route('/' , methods=['POST', 'GET'])
 def upload():
 	if request.method == 'POST':
 		file = request.files['file']
-		type (file)
-		num=[1,2,3,4,5,7]
-		media=11
-		desviacion=34.5
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			num=[1,2,3,4,5,7]
+			media=11
+			desviacion=34.5
 		return jsonify(numeros=num, media=media, desviacion=desviacion)
 	else :
 		return render_template('template.html')
