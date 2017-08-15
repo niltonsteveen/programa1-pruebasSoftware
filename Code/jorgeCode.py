562aba355556f5fd734782795b56c6d4ecff91ff
nilton <nilton.velez@udea.edu.co>
2017-08-15 13:13:02 -0500

    Test not numbers

--- Code/jorgeCode.py
+++ Code/jorgeCode.py
@@ -3,7 +3,7 @@ class Mean(object):
 	media = None
 	#
 	def __init__(self):
-		self.media=None
+		self.media=0
 	def calculateMean(self, array):
 		for x in array:
 			self.media=self.media+x
--- Code/jorgeTest.py
+++ Code/jorgeTest.py
@@ -8,5 +8,9 @@ class TestsMean(unittest.TestCase):
 		array=[]		
 		mean=self.meanObject.calculateMean(array)
 		self.assertEqual(0, mean)
+	def testNotNumbers(self):
+		array=['fda', 43, 'saf'];
+		mean=self.meanObject.calculateMean(array)
+		self.assertEqual(0, mean)
 if __name__ == '__main__':
 	unittest.main()
