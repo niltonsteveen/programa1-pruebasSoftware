f7d2bcad9d4c91098ac27f604f4ca73d4d6b7a74
nilton <nilton.velez@udea.edu.co>
2017-08-14 19:18:50 -0500

    prueba para mirar el post

--- code.py
+++ code.py
@@ -5,16 +5,14 @@ app.config['UPLOAD_FOLDER'] = 'uploads/'
 # These are the extension that we are accepting to be uploaded
 app.config['ALLOWED_EXTENSIONS'] = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-@app.route('/')
-def index():
-	return render_template('template.html')
+@app.route('/' , methods=['POST', 'GET'])
 
-@app.route('/upload', methods=['POST'])
 def upload():
 	if request.method == 'POST':
 		file = request.files['file']
 		return 'se cargo'
-		
+	else
+		return render_template('template.html')
 
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
